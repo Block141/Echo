20f3b25a258e63e98a173932a1c367a57d3d96ca
Block141 <block141@gmail.com>
2024-06-14 22:38:45 -0500

    Contianerized

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,10 +41,13 @@ MIDDLEWARE = [
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:5001",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
+    'http://localhost:5001',
+
 ]
 
 ROOT_URLCONF = "backend.urls"
